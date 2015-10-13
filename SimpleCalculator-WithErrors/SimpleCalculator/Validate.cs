338d1cb4dc5b1bcfe737a76bafdc7aacce7a1c64
Lando B <jrolandobernal@gmail.com>
2015-10-13 09:24:12 -0500

    Tried to add more Unit Tests but got two errors that I havent been able to fix. Working on it. Saving just in case.

---
+++ SimpleCalculator-WithErrors/SimpleCalculator/Validate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SimpleCalculator
+{
+    // Here is where I plan to validate that the input the user is entering is correct. Once I figure out how. For now, all is on the Calculate.cs
+    class Validate
+    {
+
+    }
+}
