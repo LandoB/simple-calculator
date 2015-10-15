22d2ae7447dab40fa8e547196bafd2376de6060d
Lando B <jrolandobernal@gmail.com>
2015-10-14 21:20:09 -0500

    Started over to understand better this assigment.

---
+++ SimpleCalculator-1/SimpleCalculator-1/Parse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator1
+{
+    public class Parse
+    {
+        private int value1;
+        private int value2;
+        private char operador;
+
+        public int Value1
+        {
+            get { return value1; }
+            set { value1 = value; }
+        }
+
+        public int Value2
+        {
+            get { return value2; }
+            set { value2 = value; }
+        }
+
+        public char Operador
+        {
+            get { return operador; }
+            set { operador = value; }
+        }
+
+        public void ReadInput(string input)
+        {
+            
+        }
+
+
+    }
+}
---
+++ SimpleCalculator-1/SimpleCalculatorTest-1/CalculateTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculatorTest_1
+{
+    [TestClass]
+    public class CalculateTest1
+    {
+
+    }
+   
+
+}
